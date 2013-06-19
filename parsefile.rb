743f0e044efcac97364ceb1a9d98984e3c6262fe
chhhris <mr.lake@gmail.com>
2013-06-19 15:04:18 -0400

    updates, not sure if they work

--- parsefile.rb
+++ parsefile.rb
@@ -7,12 +7,38 @@ load 'lib/genre.rb'
 files = Dir['data/*.mp3']
 
 jukebox_array = files.map do |parser|
-  artist = parser.split(" -")[0]
+  artist = parser.gsub(/data\//,'').split(" -")[0]
   song = parser.split(" - ")[1].split(" [")[0]
   genre = parser.split(" -")[1].split(" [")[1].split("]")[0]
   {artist: artist, song: song, genre: genre}
 end
 
-p jukebox_array
+jukebox_array.collect do |array_from_jukebox|
 
-p jukebox_array[0]
+  artist = array_from_jukebox[:artist]
+  genre = array_from_jukebox[:genre]
+  song = array_from_jukebox[:song]
+
+  
+  if Artist.all.include?(artist)
+    song = Song.new
+    artist.add_song(song)
+    
+    unless Genre.all.include?(genre)
+        genre = Genre.new
+        artist.genres.include(song.genre)
+        song.genre = genre
+    end
+  
+  else artist = Artist.new
+    artist.name = artist
+    song = Song.new
+    artist.add_song(song)
+    unless Genre.all.include?(genre)
+        genre = Genre.new
+    end
+
+  end
+end
+
+puts Artist.all
