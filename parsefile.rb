c21d714632e7ee537458e447c090855966df61f6
chhhris <mr.lake@gmail.com>
2013-06-21 13:25:01 -0400

    Minor changes

--- parsefile.rb
+++ parsefile.rb
@@ -1,44 +1,65 @@
-load 'lib/artist.rb'
-load 'lib/song.rb'
-load 'lib/genre.rb'
+require_relative 'lib/artist.rb'
+require_relative 'lib/song.rb'
+require_relative 'lib/genre.rb'
 
-# Load all mp3 files
 
-files = Dir['data/*.mp3']
+artist_list = []
+song_list = []
+genre_list = []
 
-jukebox_array = files.map do |parser|
-  artist = parser.gsub(/data\//,'').split(" -")[0]
-  song = parser.split(" - ")[1].split(" [")[0]
-  genre = parser.split(" -")[1].split(" [")[1].split("]")[0]
-  {artist: artist, song: song, genre: genre}
+Dir.foreach('data') do |item|
+    artist_name = item.split(" - ").first
+    song_name   = item.split(" - ").last.split(" [").first
+    genre_name  = item.split(" - ").last.split(" [").last.split("]").first
+    filetype    = item.split(" - ").last.split(" [").last.split("].").last
+
+    # print "Artist: #{artist}\n\t Song: #{song}\n\t\t Genre: #{genre}\n\t\t\t Filetype: #{filetype}"
+    # puts
+    # puts artist.inspect
+    # puts song.inspect
+    # puts genre.inspect
+    # puts filetype.inspect
+    # break if filetype == "mp3"
+    
+    
+    if filetype = "mp3"
+
+      # Instantiate new artist, song, and genre if they don't exist
+      artist = add_artist(artist_name, artist_list)
+      song   = add_song(song_name, song_list)
+      genre  = add_genre(genre_name, genre_list)
+
+      # Assign relationships:
+      song.genre = genre
+      artist.add_song(song)
+      
+    end
 end
 
-jukebox_array.collect do |array_from_jukebox|
 
-  artist = array_from_jukebox[:artist]
-  genre = array_from_jukebox[:genre]
-  song = array_from_jukebox[:song]
-
-  
-  if Artist.all.include?(artist)
-    song = Song.new
-    artist.add_song(song)
-    
-    unless Genre.all.include?(genre)
-        genre = Genre.new
-        artist.genres.include(song.genre)
-        song.genre = genre
-    end
-  
-  else artist = Artist.new
-    artist.name = artist
-    song = Song.new
-    artist.add_song(song)
-    unless Genre.all.include?(genre)
-        genre = Genre.new
-    end
-
+def add_artist(name, list)
+  if list.include? name
+    return Artist.all.select{|element| element.name == name}.first
+  else
+    list << name
+    return Artist.new(name)
   end
 end
 
-puts Artist.all
+def add_song(name, list)
+  if list.include? name
+    return Song.all.select{|element| element.name == name}.first
+  else
+    list << name
+    return Song.new(name)
+  end
+end
+
+def add_genre(name, list)
+  if list.include? name
+    return Genre.all.select{|element| element.name == name}.first
+  else
+    list << name
+    return Genre.new(name)
+  end
+end
